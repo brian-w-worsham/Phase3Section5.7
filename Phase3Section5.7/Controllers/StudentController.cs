7d93e75065bdfb461ba4692f36e6e9d2d7f2ecfa
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-10 08:00:27 -0500

    Add project files.

---
+++ Phase3Section5.7/Controllers/StudentController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Phase3Section5._7.Models;
+namespace Phase3Section5._7.Controllers;
+
+public static class StudentController
+{
+    public static void MapStudentEndpoints (this IEndpointRouteBuilder routes)
+    {
+        routes.MapGet("/api/Student", async (SchoolContext db) =>
+        {
+            return await db.Student.ToListAsync();
+        })
+        .WithName("GetAllStudents")
+        .Produces<List<Student>>(StatusCodes.Status200OK);
+
+        routes.MapGet("/api/Student/{id}", async (int Id, SchoolContext db) =>
+        {
+            return await db.Student.FindAsync(Id)
+                is Student model
+                    ? Results.Ok(model)
+                    : Results.NotFound();
+        })
+        .WithName("GetStudentById")
+        .Produces<Student>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+
+        routes.MapPut("/api/Student/{id}", async (int Id, Student student, SchoolContext db) =>
+        {
+            var foundModel = await db.Student.FindAsync(Id);
+
+            if (foundModel is null)
+            {
+                return Results.NotFound();
+            }
+
+            db.Update(student);
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        })
+        .WithName("UpdateStudent")
+        .Produces(StatusCodes.Status404NotFound)
+        .Produces(StatusCodes.Status204NoContent);
+
+        routes.MapPost("/api/Student/", async (Student student, SchoolContext db) =>
+        {
+            db.Student.Add(student);
+            await db.SaveChangesAsync();
+            return Results.Created($"/Students/{student.Id}", student);
+        })
+        .WithName("CreateStudent")
+        .Produces<Student>(StatusCodes.Status201Created);
+
+        routes.MapDelete("/api/Student/{id}", async (int Id, SchoolContext db) =>
+        {
+            if (await db.Student.FindAsync(Id) is Student student)
+            {
+                db.Student.Remove(student);
+                await db.SaveChangesAsync();
+                return Results.Ok(student);
+            }
+
+            return Results.NotFound();
+        })
+        .WithName("DeleteStudent")
+        .Produces<Student>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
+    }
+}
